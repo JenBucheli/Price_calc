621343c74fb5530aaf075d227fefaddb08dde6c1
Jenifer Bucheli <jenifer.bucheli@gmail.com>
2021-03-23 14:25:20 +0100

    DB conection and product class

--- Model/Product.php
+++ Model/Product.php
@@ -1,1 +1,41 @@
 <?php
+
+
+class Product
+{
+    private int $productID;
+    private string $name;
+    private int $price;
+
+    public function __construct(int $productID, string $name, string $price)
+    {
+        $this->productID = $productID;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    /**
+     * @return int
+     */
+    public function getProductID()
+    {
+        return $this->productID;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int|string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
+
