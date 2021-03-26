e337bc3e0def9ff43d2e1e54e5b61f24b870cd8f
Jenifer Bucheli <jenifer.bucheli@gmail.com>
2021-03-26 13:47:13 +0100

    view page including form

--- View/homepage.php
+++ View/homepage.php
@@ -1,60 +1,41 @@
-
-<?php
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Price Calculator</title>
-</head>
-<body>
-<section>
-    <form method="post">
-        <!-- Example single danger button -->
-        <div class="btn-group">
-            <button type="button" class="btn btn-success btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Customer
-            </button>
-            <div class="dropdown-menu">
-
-                <?php  /** @var Customer $customer */
-                foreach ((array)$customers  AS $customer):?>
-                <a class="dropdown-item" type="<?php echo $customer->getId()?>"><?php echo $customer->getLastname()?><?php echo $customer->getFirstname()?>
-                </a>
-                <?php endforeach;?>
-
+<br>
+<div class="d-flex justify-content-center">
+    <div class="form-control col-md-6" align="center">
+        <br>
+        <h1 align="center">Price Calculator</h1>
+        <form method="post">
+            <div class="form-col">
+                <p class="text-left">CUSTOMERS</p>
+                <select class="form-control form-select-lg mb-5" aria-label="Default select example">
+                <option selected>--Select Customer--</option>
+                    <?php  /** @var Customer $customer */
+                    foreach ((array)$customers  AS $customer):?>
+                    <option value= "<?php echo $customer->getId()?>"> <?php echo $customer->getLastname()?> <?php echo $customer->getFirstname()?></option>
+                    <?php endforeach;?>
+            </select>
             </div>
-        </div>
 
-        <div class="btn-group">
-            <button type="button" class="btn btn-success btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Products
-            </button>
-            <div class="dropdown-menu">
+            <div class="form-col">
+                <p class="text-left">PRODUCTS</p>
+                <select class="form-control form-select-lg mb-5" aria-label="Default select example">
+                <option selected>--Select Products--</option>
 
-                <?php  /** @var Product $product */
-                foreach ((array)$products  AS $product):?>
-                    <a class="dropdown-item" type="<?php echo $product->getProduct_ID()?>"><?php echo $product->getName()?>
-                    </a>
-                <?php endforeach;?>
-
+                    <?php  /** @var Product $product */
+                    foreach ((array)$products  AS $product):?>
+                        <option value="<?php echo $product->getProduct_ID()?>"><?php echo $product->getName()?>
+                        </option>
+                    <?php endforeach;?>
+            </select>
             </div>
-        </div>
-    </form>
+            <div class="col-xs-4 text-center">
+            <button type="submit" class="btn btn-success btn-lg md-5" id="submit" name="run">Get Price</button>
+            </div>
+            <br>
+            <h4 align="center">Your total price is (this change with the right php calls)</h4>
 
-</section>
+        </form>
+
+    </div>
+</div>
 
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
-</html>
--- index.php
+++ index.php
@@ -7,12 +7,14 @@ require 'Loader/ProductLoader.php';
 require 'Controller/HomeController.php';
 require 'config.php';
 
+require 'View/Includes/header.php';
+
 $controller = new HomeController();
 
 $controller->render($_GET, $_POST);
 
 
-
+require 'View/Includes/footer.php';
 
 
 
